d0165e58da6be349dc606bda9b82eea0b47a7379
kayaleeeee <gayeon71057@gmail.com>
2020-08-10 13:16:09 +0900

    Install package (sass, npm, classnames) & edit scss style

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useRef, useCallback } from "react";
+import TodoInsert from "./TodoInsert";
+import TodoTemplate from "./TodoTemplate";
+import TodoList from "./TodoList";
+
+const App = () => {
+	const [todos, setTodos] = useState([
+		{ id: 1, text: "react 복습하기", checked: true },
+		{ id: 2, text: "정처기 공부하기", checked: true },
+		{ id: 3, text: "react app 만들기", checked: false },
+	]);
+
+	const nextId = useRef(4);
+	const onInsert = useCallback(
+		(text) => {
+			const todo = {
+				id: nextId.current,
+				text,
+				checked: false,
+			};
+			setTodos(todos.concat(todo));
+			nextId.current += 1;
+		},
+		[todos]
+	);
+	const onRemove = useCallback(
+		(id) => {
+			setTodos(todos.filter((todo) => todo.id !== id));
+		},
+		[todos]
+	);
+
+	const onToggle = useCallback(
+		(id) => {
+			setTodos(
+				todos.map((todo) =>
+					todo.id === id ? { ...todo, checked: !todo.checked } : todo
+				)
+			);
+		},
+		[todos]
+	);
+	return (
+		<TodoTemplate>
+			<TodoInsert onInsert={onInsert} />
+			<TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+		</TodoTemplate>
+	);
+};
+
+export default App;
